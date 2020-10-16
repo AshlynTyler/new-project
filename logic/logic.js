919a411c800ab5eb97a9108936abf2bfa1ba42b6
Cyber Witch <no-reply>
2020-10-16 15:19:34 -0600

    created the most bare bones incrimental possible

--- logic/logic.js
+++ logic/logic.js
@@ -8,8 +8,31 @@ let generator = {
     buy: function(){
 
         this.amount ++;
-        this.cost = (this.amount ** 1.25) * 10
+        this.cost = Math.round(((this.amount + 1) ** 1.25) * 10)
     }
     
 }
 
+let numberCounter = document.getElementById("number-counter")
+
+let generatorButton = document.getElementById("generator-button")
+
+let generatorAmount = document.getElementById("generator-amount")
+
+
+generatorButton.addEventListener("click", function(){
+    if(number >= generator.cost){
+        number -= generator.cost
+        generator.buy()
+
+        numberCounter.textContent = "number:" + number.toString()
+        generatorAmount.textContent = "generators:" + generator.amount.toString()
+        generatorButton.textContent = generator.cost.toString()
+    }
+})
+
+setInterval(function(){
+    number += 1 + (generator.amount * generator.value * generator.multiplyer)
+    console.log(number)
+    numberCounter.textContent = "number:" + number.toString()
+}, 1000)
